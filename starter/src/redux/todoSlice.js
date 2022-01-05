e7d53406dce11bdb84ff1fc8d4bdc0320a681334
Jorge Nguiraze <jjorge@technoplus.co.mz>
2022-01-05 12:52:07 +0200

    FUncoes para apagar os tudos

--- starter/src/redux/todoSlice.js
+++ starter/src/redux/todoSlice.js
@@ -18,11 +18,18 @@ export const todoSlice = createSlice({
 			};
 			state.push(todo);
 		},
+    toggleComplete: (state, action) => {
+			const index = state.findIndex((todo) => todo.id === action.payload.id);
+			state[index].completed = action.payload.completed;
+		},
+    deleteTodo: (state, action) => {
+			return state.filter((todo) => todo.id !== action.payload.id);
+		},
 
 	},
 });
 
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
