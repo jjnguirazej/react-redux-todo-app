0e10cb612bd35534463eacbbdd42e24e91f5438d
Jorge Nguiraze <jjorge@technoplus.co.mz>
2022-01-05 10:51:28 +0200

    config inicial

--- starter/src/redux/store.js
+++ starter/src/redux/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from './todoSlice';
 
 export default configureStore({
-	reducer: {
-		todos: todoReducer,
-	},
+	reducer: {},
 });
