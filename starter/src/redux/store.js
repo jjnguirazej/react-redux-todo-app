b4f45f0f1beafd0634b91ab7977a6acccd304056
Jorge Nguiraze <jjorge@technoplus.co.mz>
2022-01-05 12:16:12 +0200

    Listagem de todos usando useSelector, Adicionar Todo usando Dispach

--- starter/src/redux/store.js
+++ starter/src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import todoReducer from './todoSlice';
 export default configureStore({
-	reducer: {},
+	reducer: {
+    todos: todoReducer,
+  },
 });
